454e340349b175118d75754d931d85cb95ea1a16
Evan-Ma <majingtao9506@outlook.com>
2019-10-21 22:50:15 +0800

    First

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="R_bookWY.MvcApplication" Language="C#" %>
---
+++ Models/Receive.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace R_bookWY.Models
+{
+    public class dhl
+    {
+        public string id { get; set; }
+        public string title { get; set; }
+        public string icon { get; set; }
+        public string spread { get; set; }
+        public string href { get; set; }
+        public dhl children { get; set; }
+
+    }
+}
