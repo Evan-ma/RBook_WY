8f3d350e9319567ea9265ce454e6ebb529dc19d9
Evan-Ma <majingtao9506@outlook.com>
2019-10-22 13:36:20 +0800

    导航加载

---
+++ Class/CY.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+
+namespace R_bookWY.Class
+{
+    public class CY
+    {
+        public DataTable ZxSql(string sql)
+        {
+            DataTable dt = new DataTable();
+            string error = "";
+            //用于保存子菜单
+            try
+            {
+                DataSet ds = new DataSet();
+                //如果是查询语句
+                string sqlConnection = "Data Source=" + System.AppDomain.CurrentDomain.BaseDirectory + "//DB/book.db" + ";";
+                SQLiteConnection sqlc = new SQLiteConnection(sqlConnection);
+                SQLiteDataAdapter sdp = new SQLiteDataAdapter();
+                try
+                {
+                    SQLiteCommand cmd = new SQLiteCommand(sql, sqlc);
+                    sdp.SelectCommand = cmd;
+                    sdp.Fill(ds);
+                    sdp.Dispose();
+                    dt = ds.Tables[0];
+                }
+                catch (Exception ex)
+                {
+                    error += ex.Message.ToString();
+                }
+                sqlc.Close();
+
+            }
+            catch (Exception ex)
+            {
+                error += ex.Message.ToString();
+            }
+            if (error != "")
+            {
+                dt.Clear();
+                dt.Columns.Add("执行情况");
+                dt.Columns.Add("错误信息");
+                DataRow dr = dt.NewRow();
+                dr["执行情况"] = "失败";
+                dr["错误信息"] = error;
+                dt.Rows.Add(dr);
+            }
+            return dt; 
+        }
+    }
+    public class MeunInfo
+    {
+        public string id;
+        //菜单标题
+        public string meunTitle;
+        //菜单地址
+        public string meunUrl;
+        //菜单状态
+        public string meunStatus;
+        //菜单标识
+        public string meunParent;
+        //菜单排序
+        public string meunSort;
+        //子菜单集合
+        public List<MeunInfo> childrenList;
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using R_bookWY.Class;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data;
+using System.Data.SQLite;
 using System.Web.Mvc;
 
 namespace R_bookWY.Controllers
@@ -12,5 +12,49 @@ namespace R_bookWY.Controllers
         {
             return View();
         }
+        public ActionResult Index_1()
+        {
+            return View();
+        }
+        public ActionResult getParentMeun()
+        {
+            //获取所有的菜单(包括子菜单和父级菜单)
+            List<MeunInfo> list = new List<MeunInfo>();
+            CY C = new CY();
+            string sql = "select * from t_moudle where upcode = '-1'";
+            DataTable dt = C.ZxSql(sql);
+            for (int i = 0; i < dt.Rows.Count; i++)
+
+            {
+                MeunInfo mi = new MeunInfo();
+                {
+                    mi.id = dt.Rows[i]["id"].ToString();
+                    mi.meunTitle = dt.Rows[i]["name"].ToString();
+                    mi.meunUrl = dt.Rows[i]["url"].ToString();
+                    mi.meunParent = dt.Rows[i]["upcode"].ToString();
+                    mi.childrenList = new List<MeunInfo>();
+                    list.Add(mi);
+                }
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                sql = "select * from t_moudle where upcode = '"+ list[i].id + "'";
+                DataTable dtp = C.ZxSql(sql);
+                for (int p = 0; p < dtp.Rows.Count; p++)
+                {
+                    MeunInfo mi = new MeunInfo();
+                    mi.id = dtp.Rows[p]["id"].ToString();
+                    mi.meunTitle = dtp.Rows[p]["name"].ToString();
+                    mi.meunUrl = dtp.Rows[p]["url"].ToString();
+                    mi.meunParent = dtp.Rows[p]["upcode"].ToString();
+                    list[i].childrenList.Add(mi);
+                }
+            }
+            JsonResult result = new JsonResult();
+            result.Data = list;
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return result;
+        }
     }
+
 }
